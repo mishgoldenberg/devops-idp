54baa268c20222b5e7ff7ce6170ca70f2bc21ad3
Elizabeth <lizaczon@gmail.com>
2026-03-10 01:40:02 +0200

    Changed front dockerfile

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,9 +7,6 @@ const nextConfig = {
     NEXT_PUBLIC_API_GATEWAY_URL: process.env.NEXT_PUBLIC_API_GATEWAY_URL || 'http://localhost:8000',
     NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME || 'DevOps Control Center',
   },
-  images: {
-    domains: ['localhost'],
-  },
 }
 
 module.exports = nextConfig
