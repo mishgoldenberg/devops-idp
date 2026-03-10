fb1ed8bcd3bbe25d54922d7173743e3dd083479c
Elizabeth <lizaczon@gmail.com>
2026-03-10 17:34:24 +0200

    deployment works, namespace dev is up

--- infrastructure/k8s/base/database-replace-later-helm/seeds/04_approval_rules.sql
+++
@@ -1,7 +0,0 @@
--- Seed approval workflow rules
-
-INSERT INTO approval_rules (request_type, min_approver_role_level, approver_count) VALUES
-('ADO_PROJECT_CREATE', 3, 1),        -- Branch Head or above
-('SONAR_PR_SCANNING_ENABLE', 4, 1),  -- Head of Section or above
-('AI_MODEL_ACCESS', 1, 1);           -- Platform Admin only
-
